125467e7116527cc38f5988d75e7108ff2743a6e
Vid Čufar <vid.cufar@gmail.com>
2021-04-14 22:31:58 +0200

    Navigation styles & login route

--- client/src/views/App.tsx
+++ client/src/views/App.tsx
@@ -1,28 +1,45 @@
-import React, { FC } from 'react';
-import { Layout, Menu } from 'antd';
+import React, { FC, useState } from 'react';
+import { Avatar, Col, Layout, Menu, Row } from 'antd';
+import { HomeOutlined, UserOutlined } from '@ant-design/icons';
 import { Link, Route } from 'react-router-dom';
 import styled from '@emotion/styled';
 
 import { Competitions, Home, Results } from './index';
 
 import '../assets/styles/App.css';
-import { ROUTE } from '../constants';
+import { COLOR, ROUTE } from '../constants';
 
 const App: FC = () => {
+  const [currentTab, setCurrentTab] = useState('home');
+
   return (
     <Container>
       <Layout.Header>
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-          <Menu.Item key={1}>
-            <Link to={ROUTE.HOME}>Home</Link>
-          </Menu.Item>
-          <Menu.Item key={2}>
-            <Link to={ROUTE.COMPETITIONS}>Competitions</Link>
-          </Menu.Item>
-          <Menu.Item key={3}>
-            <Link to={ROUTE.RESULTS}>Results</Link>
-          </Menu.Item>
-        </Menu>
+        <Row justify="space-between">
+          <Col>
+            <Menu
+              onClick={(e) => setCurrentTab(e.key.toString())}
+              selectedKeys={[currentTab]}
+              theme="dark"
+              mode="horizontal"
+            >
+              <Menu.Item key="home" icon={<HomeOutlined />}>
+                <Link to={ROUTE.HOME}>Home</Link>
+              </Menu.Item>
+              <Menu.Item key="competitions">
+                <Link to={ROUTE.COMPETITIONS}>Competitions</Link>
+              </Menu.Item>
+              <Menu.Item key="results">
+                <Link to={ROUTE.RESULTS}>Results</Link>
+              </Menu.Item>
+            </Menu>
+          </Col>
+          <Col>
+            <Link to={ROUTE.LOGIN}>
+              <UserAvatar icon={<UserOutlined />} />
+            </Link>
+          </Col>
+        </Row>
       </Layout.Header>
       <Content>
         <Route exact path={ROUTE.HOME} component={Home} />
@@ -52,4 +69,8 @@ const Content = styled(Layout.Content)`
 
 const Footer = styled(Layout.Footer)`
   flex-shrink: 0;
+`;
+
+const UserAvatar = styled(Avatar)`
+  background: ${COLOR.LIGHT};
 `;
