2864f35feaed9220699a1ef0230296966b69d83b
Vid Čufar <vid.cufar@gmail.com>
2021-04-18 17:44:49 +0200

    Moved logout logic to utilities

--- client/src/apollo/cache.ts
+++ client/src/apollo/cache.ts
@@ -7,3 +7,5 @@ export const isLoggedInVar = makeVar<boolean>(!!localStorage.getItem('token'));
 export const loggedUserId = makeVar<string | null>(
   localStorage.getItem('publicId'),
 );
+
+export const loggedUserInfo = makeVar(null);
