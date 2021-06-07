240979d7eb9ddddd9355b23bd3b0a49d556e717d
Vid Čufar <vid.cufar@gmail.com>
2021-06-07 17:29:41 +0200

    Basic results table with sorting

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -8,3 +8,16 @@ export type ColorVariant = 'dark' | 'light';
   Score
 */
 export type ScoreType = 'top' | 'zone' | 'attempt';
+
+/*
+  Result
+*/
+export interface ResultRecord {
+  rank: number;
+  name: string;
+  country: string;
+  club: string;
+  qualification: string;
+  semiFinal: string;
+  final: string;
+}
