64d07f62fdce2c228db7281d499cd7a42cc94b25
Vid Čufar <vid.cufar@gmail.com>
2021-06-11 17:26:23 +0200

    Competitions display
    Displays seasons & their competitions, sort by season year

--- client/src/constants/Route.ts
+++ client/src/constants/Route.ts
@@ -3,14 +3,15 @@
  */
 // prettier-ignore
 export const ROUTE = {
-  HOME:         '/',
-  LOGIN:        '/login',
-  COMPETITIONS: '/competitions',
-  CALENDAR:     '/calendar',
-  RESULTS:      '/results',
-  ATHLETES:     '/athletes',
-  DASHBOARD:    '/dashboard',
-  SCORE:        '/dashboard/score',
-  SCORE_ID:     '/dashboard/score/:id',
-  ROUTES:       '/dashboard/routes',
+  HOME:            '/',
+  LOGIN:           '/login',
+  COMPETITIONS:    '/competitions',
+  COMPETITIONS_ID: '/competitions/:id',
+  CALENDAR:        '/calendar',
+  RESULTS:         '/results',
+  ATHLETES:        '/athletes',
+  DASHBOARD:       '/dashboard',
+  SCORE:           '/dashboard/score',
+  SCORE_ID:        '/dashboard/score/:id',
+  ROUTES:          '/dashboard/routes',
 };
